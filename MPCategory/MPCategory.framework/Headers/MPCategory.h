e0f9239d0c3ca0ffe07c1a9fc27c9dcb3bc6f472
panfei mao <843158788@qq.com>
2018-03-22 22:41:33 +0800

    新建

---
+++ MPCategory/MPCategory.framework/Headers/MPCategory.h
@@ -0,0 +1,16 @@
+//
+//  MPCategory.h
+//  MPCategory
+//
+//  Created by panfei mao on 2018/3/22.
+//  Copyright © 2018年 panfei mao. All rights reserved.
+//
+
+#ifndef MPCategory_h
+#define MPCategory_h
+
+#import "UIColor+HEX.h"
+#import "UILabel+instance.h"
+#import "UILabel+Attribute.h"
+
+#endif /* MPCategory_h */
